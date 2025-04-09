e51ef35c69f9e80204c1e493f2096f41b7b7e3e6
OmarChouchane <omar.ch52831@gmail.com>
2025-04-09 03:24:54 +0100

    navbar and side bar with some content colors

--- js/main.js
+++ js/main.js
@@ -73,17 +73,17 @@
             datasets: [{
                     label: "USA",
                     data: [15, 30, 55, 65, 60, 80, 95],
-                    backgroundColor: "rgba(235, 22, 22, .7)"
+                    backgroundColor: "rgb(0, 132, 207)"
                 },
                 {
                     label: "UK",
                     data: [8, 35, 40, 60, 70, 55, 75],
-                    backgroundColor: "rgba(235, 22, 22, .5)"
+                    backgroundColor:"rgb(1, 86, 135)"
                 },
                 {
                     label: "AU",
                     data: [12, 25, 45, 55, 65, 70, 60],
-                    backgroundColor: "rgba(235, 22, 22, .3)"
+                    backgroundColor:"rgb(0, 58, 92)"
                 }
             ]
             },
@@ -102,13 +102,13 @@
             datasets: [{
                     label: "Salse",
                     data: [15, 30, 55, 45, 70, 65, 85],
-                    backgroundColor: "rgba(235, 22, 22, .7)",
+                    backgroundColor: "rgb(0, 132, 207)",
                     fill: true
                 },
                 {
                     label: "Revenue",
                     data: [99, 135, 170, 130, 190, 180, 270],
-                    backgroundColor: "rgba(235, 22, 22, .5)",
+                    backgroundColor:"rgb(0, 58, 92)",
                     fill: true
                 }
             ]
